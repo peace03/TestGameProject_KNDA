f17d19fc3f35912e28df5ebd353528b4d03f107d
YunPyeongHwa <64591006+peace03@users.noreply.github.com>
2026-09-10 17:55:18 +0900

    feat: 도형 프리팹 및 색상 변경/페이드 스크립트 추가
    - Circle/Square/Star/Triangle/ChangeColorBar 프리팹 추가 (03_Prefabs)
    - ChangeColorBarController, FadeEffect 스크립트 추가
    - CameraOrthographicSize에 자동 조정 여부 토글 옵션 추가
    - 사용하지 않는 VARCO3DImports usdz 리소스 정리
    - SampleScene에 신규 프리팹/스크립트 참조 반영

--- ColorRun_MBC/Assets/Project/05_Scripts/CameraOrthographicSize.cs
+++ ColorRun_MBC/Assets/Project/05_Scripts/CameraOrthographicSize.cs
@@ -2,9 +2,14 @@ using UnityEngine;
 
 public class CameraOrthographicSize : MonoBehaviour
 {
+    [SerializeField] private bool autoAdjustOrthographicSize = false;
+
     private void Awake()
     {
-        float screenAspectRatio = (float)(Screen.width / Screen.height);
+        // Preserve the camera size configured in the scene unless explicitly enabled.
+        if (!autoAdjustOrthographicSize) return;
+
+        float screenAspectRatio = (float)Screen.width / Screen.height;
         float orthographicSize = (float)(6 - (screenAspectRatio - 0.485f) * 11f);
         if(orthographicSize < 4)
         {
---
+++ ColorRun_MBC/Assets/Project/05_Scripts/ChangeColorBarController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class ChangeColorBarController : MonoBehaviour
+{
+    private SpriteRenderer[] renderers;
+    private float fadeTime = 0.4f;
+
+    private void Awake()
+    {
+        renderers = transform.GetComponentsInChildren<SpriteRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(nameof(OnFadeLoop));
+    }
+    private void OnDisable()
+    {
+        StopCoroutine(nameof(OnFadeLoop));
+    }
+
+    private IEnumerator OnFadeLoop()
+    {
+        var waitForSeconds = new WaitForSeconds(fadeTime);
+        while (true)
+        {
+            for(int i = 0; i < renderers.Length; i++)
+            {
+                StartCoroutine(FadeEffect.Fade(renderers[i], 1f, 0f, fadeTime));
+            }
+            yield return waitForSeconds;
+
+            for(int i = 0; i < renderers.Length; i++)
+            {
+                StartCoroutine(FadeEffect.Fade(renderers[i], 0f, 1f, fadeTime));
+            }
+            yield return waitForSeconds;
+        }
+    }
+}

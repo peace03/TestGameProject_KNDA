5f9309f17c6d43ccd3e5eb740f66ccbb59e17ff5
YunPyeongHwa <64591006+peace03@users.noreply.github.com>
2026-09-11 15:29:31 +0900

    feat: 이동/크기/회전 오브젝트 배리언트 프리팹 및 스크립트 추가
    - MoveHorizontal, Resizer, RotateToAxis 스크립트 추가
    - Circle/Square/Triangle Mover, Resizer, Rotator 프리팹 배리언트 추가 (03_Prefabs/DefalultObject/Varient)
    - SampleScene에 신규 프리팹 참조 반영

---
+++ ColorRun_MBC/Assets/Project/05_Scripts/MoveHorizontal.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using UnityEngine;
+
+public class MoveHorizontal : MonoBehaviour
+{
+    [SerializeField] private float start;
+    [SerializeField] private float end;
+    [SerializeField] private float unitPerSecond = 1f;
+
+    private void OnEnable()
+    {
+        StartCoroutine(nameof(Process));
+    }
+    private void OnDisable()
+    {
+        StopCoroutine(nameof(Process));
+    }
+
+    private IEnumerator Process()
+    {
+        while (true)
+        {
+            yield return StartCoroutine(MoveTo(start, end));
+            yield return StartCoroutine(MoveTo(end, start));
+        }
+    }
+
+    private IEnumerator MoveTo(float start, float end)
+    {
+        float percent = 0f;
+        float distance = Mathf.Abs(end - start);
+        float moveTime = distance * unitPerSecond;
+
+        while (percent < 1)
+        {
+            percent += Time.deltaTime / moveTime;
+            Vector3 position = transform.position;
+            position.x = Mathf.Lerp(start, end, percent);
+
+            yield return null;
+        }
+    }
+}
---
+++ ColorRun_MBC/Assets/Project/05_Scripts/Resizer.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+
+public class Resizer : MonoBehaviour
+{
+    [SerializeField] private Vector3 start;
+    [SerializeField] private Vector3 end;
+    [SerializeField] private float resizeTime = 1f;
+    
+    private void OnEnable()
+    {
+        StartCoroutine(nameof(Process));
+    }
+    private void OnDisable()
+    {
+        StopCoroutine(nameof(Process));
+    }
+
+    private IEnumerator Process()
+    {
+        while (true)
+        {
+            yield return StartCoroutine(ScaleTo(start, end));
+            yield return StartCoroutine(ScaleTo(end, start));
+        }
+    }
+
+    private IEnumerator ScaleTo(Vector3 start, Vector3 end)
+    {
+        float percent = 0f;
+        while (percent < 1)
+        {
+            percent += Time.deltaTime / resizeTime;
+            transform.localScale = Vector3.Lerp(start, end, percent);
+            yield return null;
+        }
+    }
+}
